88e489b14538dc017686335929f94a4c9708409b
Juan Lucero <jlucero@alhpaworks.tech>
2021-12-13 11:15:21 -0800

    feat(greeting card): develop greeting card layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import GreetingCard from './Components/GreetingCard';
+import { useState } from 'react';
 
 function App() {
+  const [greeting, setGreeting] = useState('Dear Mom,');
+  const [body, setBody] = useState('Have a comforting and joyous holliday!');
+  const [closing, setClosing] = useState('Love, your son Juan');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GreetingCard
+      greeting={greeting}
+      body={body}
+      closing={closing}
+    />
   );
 }
 
---
+++ src/Components/GreetingCard.js
@@ -0,0 +1,24 @@
+import coffee_and_slippers from '../images/coffee_and_slippers.webp';
+import '../App.css';
+
+function GreetingCard({ greeting, body, closing }) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={coffee_and_slippers} className="App-logo" alt="coffee and slippers image" />
+        <p>
+          {greeting}
+        </p>
+        <p>{body}</p>
+        <p>{closing}</p>
+        <button
+          className="App-link"
+        >
+          Reshare This Card
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default GreetingCard;
