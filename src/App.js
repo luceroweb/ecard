f3aa0a1f3fb293ccc2b52d7530bc77ca7258bbe3
Juan Lucero <jlucero@alhpaworks.tech>
2021-12-14 10:14:13 -0800

    feat(customize form): add customize form fields #9

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CustomizeMessage from './Components/CustomizeMessage';
 import { useState } from 'react';
 
 function App() {
+  const [email, setEmail] = useState('');
   const [greeting, setGreeting] = useState('Dear Mom,');
   const [body, setBody] = useState('Have a comforting and joyous holliday!');
   const [closing, setClosing] = useState('Love, your son Juan');
@@ -12,7 +13,16 @@ function App() {
   return (
     <>
     {customize
-      ? <CustomizeMessage />
+      ? <CustomizeMessage
+        email={email}
+        setEmail={setEmail}
+        greeting={greeting}
+        setGreeting={setGreeting}
+        body={body}
+        setBody={setBody}
+        closing={closing}
+        setClosing={setClosing}
+      />
       : <GreetingCard
         greeting={greeting}
         body={body}
--- src/Components/CustomizeMessage.js
+++ src/Components/CustomizeMessage.js
@@ -1,6 +1,6 @@
 import coffee_and_slippers from '../images/coffee_and_slippers.webp';
 
-export default function CustomizeMessage() {
+export default function CustomizeMessage({ email, setEmail, greeting, setGreeting, body, setBody, closing, setClosing }) {
 
   return(
     <>
@@ -9,6 +9,54 @@ export default function CustomizeMessage() {
         <p><strong>How to Reshare:</strong><br />
         Write your own custom message below.</p>
       </div>
+      <form>
+
+        <div>
+          <label for="email">Recipient Email</label><br />
+          <input
+            name="email"
+            type="text"
+            value={email}
+            onChange={setEmail}
+            placeholder="Recipient Email"
+          />
+        </div>
+
+        <div>
+          <label for="greeting">Greeting</label><br />
+          <input
+            name="greeting"
+            type="text"
+            value={greeting}
+            onChange={setGreeting}
+            placeholder="Greeting"
+          />
+        </div>
+
+        <div>
+          <label for="body">Body</label><br />
+          <textarea
+            name="body"
+            value={body}
+            onChange={setBody}
+            placeholder="Body"
+            rows="3"
+          >
+          </textarea>
+        </div>
+
+        <div>
+          <label for="closing">Closing</label><br />
+          <input
+            name="closing"
+            type="text"
+            value={closing}
+            onChange={setClosing}
+            placeholder="Closing"
+          />
+        </div>
+
+      </form>
     </>
   )
 }
