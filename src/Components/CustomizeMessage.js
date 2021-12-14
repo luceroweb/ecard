583c3d4e6c75f494db1c3b0657fff2ae0a47e3fb
Juan Lucero <jlucero@alhpaworks.tech>
2021-12-14 09:50:20 -0800

    feat(instructions): add instructions to resend form

--- src/Components/CustomizeMessage.js
+++ src/Components/CustomizeMessage.js
@@ -1,7 +1,14 @@
+import coffee_and_slippers from '../images/coffee_and_slippers.webp';
 
 export default function CustomizeMessage() {
 
   return(
-    <p>Customize Message</p>
+    <>
+      <div className="instructions">
+        <img src={coffee_and_slippers} alt="coffee and slippers image" className="thumb" />
+        <p><strong>How to Reshare:</strong><br />
+        Write your own custom message below.</p>
+      </div>
+    </>
   )
 }
